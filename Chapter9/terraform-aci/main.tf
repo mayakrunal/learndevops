65621674da973bbe957fca0ceb8bc8f80f8d4d37
mayakrunal <maya.krunal@gmail.com>
2024-04-11 18:21:11 +0800

    update terraform main & clod-init for vmss

--- Chapter9/terraform-aci/main.tf
+++ Chapter9/terraform-aci/main.tf
@@ -6,7 +6,7 @@ resource "azurerm_container_group" "aci-app-demobook" {
   name                = "aci-app-demobook"
   location            = azurerm_resource_group.acidemobookken.location
   resource_group_name = azurerm_resource_group.acidemobookken.name
-  ip_address_type     = "public"
+  ip_address_type     = "Public"
   dns_name_label      = "demobook-ken"
   os_type             = "Linux"
 
